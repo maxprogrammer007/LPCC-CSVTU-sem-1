03778fb9082fd3db6c0e03c954c3fd64959b4eec
Abhinav Shukla <67401627+maxprogrammer007@users.noreply.github.com>
2024-01-09 08:15:13 +0530

    Just found interesting way of writing array

---
+++ Interesting problems/this also works.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+int main() {
+    int arr[8]={1,2,3,4,5,6,7,0};
+    
+    for(int i = 0 ; i < 8 ; i++)
+    {
+        printf("%s : %d \n","Okay here it is ", i[arr]);
+    }
+    return 0;
+}
