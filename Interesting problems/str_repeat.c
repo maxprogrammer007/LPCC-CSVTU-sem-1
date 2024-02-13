2b7e77e5abf5f8bc9726560d40092cdcdfb7a45e
Abhinav Shukla <67401627+maxprogrammer007@users.noreply.github.com>
2024-02-13 13:43:29 +0530

    made a program to repeat the string

---
+++ Interesting problems/str_repeat.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+    char str[100];
+    gets(str);
+
+    int length = strlen(str)/4;
+
+    int itr;
+    printf("How many times you want to repeat the string : ");
+    scanf("%d", &itr);
+
+    int f_length = length * itr;
+
+    char repeat_string[f_length];
+
+    sprintf(repeat_string, "%s%s", str);
+
+    for (int i = 0; i < itr; i++)
+    {
+
+        printf("%s", repeat_string);
+    }
+
+    return 0;
+}
